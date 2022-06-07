f2909879284a5867018c9590436a43bfd6581114
Maksim Yavlyanskiy <max_yav@csltd.com.ua>
2022-06-07 11:43:09 +0300

    loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core'
+import {HttpClient} from '@angular/common/http'
+import {delay} from "rxjs";
+
+export interface Todo {
+  completed: boolean
+  title: string
+  id?: number
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'rest-test';
+export class AppComponent implements OnInit {
+
+  todos: Todo[] = []
+  todoTitle = ''
+  loading = false
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.addFetch()
+  }
+
+  addTodo() {
+    if (!this.todoTitle.trim()){
+      return
+    }
+
+    const newTodo:Todo = {
+      title: this.todoTitle,
+      completed: false
+    }
+
+    this.http.post<Todo>("https://jsonplaceholder.typicode.com/todos",newTodo)
+      .subscribe(todo => {
+        this.todos.push(todo)
+        this.todoTitle = ''
+      })
+  }
+
+  addFetch() {
+    this.loading = true
+    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .pipe(delay(1500))
+      .subscribe(todos => {
+        this.todos = todos
+        this.loading = false
+      })
+  }
 }
